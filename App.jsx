f007c7809a3c85f3996412720153c1003f9ecc4b
Raghulbalaji18 <167724093+Raghulbalaji18@users.noreply.github.com>
2025-05-15 08:47:40 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Support from "./component/Support"; // Import your Support component
+
+function App() {
+  return (
+    <div className="App">
+      <Support />
+    </div>
+  );
+}
+
+export default App;
